df805a45a419ad9e52e27ed49adf979f0c0c5a1e
nieves <blanquita.nj26@hotmail.com>
2016-03-15 00:17:40 -0500

    segundo proyecto web

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+	<<meta charset="utf-8">
+	<<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
+	<link >></title>
+	<link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+<div class="contenedor">
+<nav>
+	<ul class= "menu-lista">
+			<li class="menu-item">
+			<a href="" class="menu-link">INICIO</a>
+			</li>
+			<li class="menu-item">
+			<a href=""class="menu-link">GALERIA</a>
+			</li>
+			<li class="menu-item">
+			<a href="" class="menu-link">BLOG</a>
+			</li>
+	<li class="menu-item">
+	<a href="" class="menu-link">ANUNCIOS</a>
+	</li>
+	<li class="menu-item">
+	<a href="" class="menu-link">CONTACTENOS</a>
+	
+	</li>
+	</nav>	
+	<p>Gracias por tu amistad y recuerda que siempre cuentas conmigo</p>
+	<div class="slider">
+	<img src="blanca1.jpg" alt="1000"  width="900">
+	<div class="slider">
+	<h1>ESTA NIÑA BONITA SOY YO</h1>
+    <p>La amistad es un regalo que no se compra ni se vende </p>
+
+	<div class="slider">
+	<img src="blanca3.JPG" height="160" nidth"140">
+	<img src="blanca4.JPG" height="160" nidth"140">
+	<img src="blanca5.jpg" height="160" nidth"140">
+</body>
+</html>
